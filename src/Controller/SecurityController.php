bb9d7b10412656ab577735f5c7d204d092b02813
hugo schellinger <hugo.schellinger@gmail.com>
2023-01-28 16:42:48 +0100

    Début email

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 #[Route('/api', name: 'api_')]
 class SecurityController extends AbstractController
@@ -22,7 +24,24 @@ class SecurityController extends AbstractController
         $this->userRepository = $userRepository;
     }
 
-    #[Route('/register', name: 'register')]
+    #[Route('/mail', name: 'mail')]
+    public function mail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+        // email address as a simple string
+        ->from('hschellinger@hotmail.fr')
+        ->html('<p>Lorem ipsum...</p>')
+        ->to('hugo.schellinger@gmail.com')
+        ->subject('Time for Symfony Mailer!')
+        ->text('Sending emails is fun again!')
+        ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        return $this->json(["message"=>"OK"],200);
+    }
+
+    #[Route('/register', name: 'register',methods:["POST"])]
     public function registration(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $email = $request->toArray()["email"] ?? null;
